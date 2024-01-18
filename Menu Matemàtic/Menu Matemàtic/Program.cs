2531b9f89e0031b89cefc8fa7de1fab883e3ef82
Eric Saez <saezeric37@gmail.com>
2024-01-18 11:58:39 +0100

    Te lo juro que es la final

--- Menu Matemàtic/Menu Matemàtic/Program.cs
+++ Menu Matemàtic/Menu Matemàtic/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using System.Data;
+using System.Drawing;
 using System.Net.NetworkInformation;
 
 namespace Menu_Matemàtic
@@ -11,110 +12,116 @@ namespace Menu_Matemàtic
         static void Main(string[] args)
         {
             // Definim variables
-            char opcio;
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Benvingut al menu matemàtic!");
-            Console.ResetColor();
-            Console.WriteLine("1. Màxim");
-            Console.WriteLine("2. MCD");
-            Console.WriteLine("3. MCM");
-            Console.WriteLine("4. Factorial");
-            Console.WriteLine("5. Combinatori");
-            Console.WriteLine("6. MostrarDivisorMajor");
-            Console.WriteLine("7. EsPrimer");
-            Console.WriteLine("8. NPrimersPrimers");
-            Console.WriteLine("q. Sortir");
-            Console.Write("Entra un valor: ");
-            opcio = Console.ReadKey().KeyChar;
-            Console.WriteLine();
-            switch (opcio)
+            char opcio = ' ';
+            while (opcio != 'q')
             {
-                case '1':
-                    Console.Clear();
-                    Console.WriteLine("Entra el primer número");
-                    int num1 = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine("Entra el segon número");
-                    int num2 = Convert.ToInt32(Console.ReadLine());
-                    int maxim = MaximCalcul(num1, num2);
-                    Console.Clear();
-                    Console.WriteLine($"El numero més gran entre {num1} i {num2} és {maxim}");
-                    Return();
-                    break;
-                case '2':
-                    Console.Clear();
-                    Console.WriteLine("Entra el primer número");
-                    num1 = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine("Entra el segon número");
-                    num2 = Convert.ToInt32(Console.ReadLine());
-                    int resultat = MCDCalcul(num1, num2);
-                    Console.Clear();
-                    Console.WriteLine($"El MCD de  {num1} i {num2} és {resultat}");
-                    Return();
-                    break;
-                case '3':
-                    Console.Clear();
-                    Console.WriteLine("Entra el primer número");
-                    num1 = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine("Entra el segon número");
-                    num2 = Convert.ToInt32(Console.ReadLine());
-                    resultat = MCMCalcul(num1, num2);
-                    Console.Clear();
-                    Console.WriteLine($"El MCM de  {num1} i {num2} és {resultat}");
-                    Return();
-                    break;
-                case '4':
-                    Console.Clear();
-                    Console.WriteLine("Entra un número");
-                    num1 = Convert.ToInt32(Console.ReadLine());
-                    resultat = FactorialCalcul(num1);
-                    Console.Clear();
-                    Console.WriteLine($"El factorial de {num1} és {resultat}");
-                    break;
-                case '5':
-                    Console.Clear();
-                    Console.WriteLine("Escriu dos numeros dels que es faran una operacio combinatoria");
-                    num1 = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine("Escriu el segon numero");
-                    num2 = Convert.ToInt32(Console.ReadLine());
-                    resultat = Combinatori(num1, num2);
-                    Console.Clear();
-                    Console.WriteLine($"El combinatori de  {num1} i {num2} és {resultat}");
-                    Return();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Benvingut al menu matemàtic!");
+                Console.ResetColor();
+                Console.WriteLine("1. Màxim");
+                Console.WriteLine("2. MCD");
+                Console.WriteLine("3. MCM");
+                Console.WriteLine("4. Factorial");
+                Console.WriteLine("5. Combinatori");
+                Console.WriteLine("6. MostrarDivisorMajor");
+                Console.WriteLine("7. EsPrimer");
+                Console.WriteLine("8. NPrimersPrimers");
+                Console.WriteLine("q. Sortir");
+                Console.Write("Entra un valor: ");
+                opcio = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+                switch (opcio)
+                {
+                    case '1':
+                        Console.Clear();
+                        Console.WriteLine("Entra el primer número");
+                        int num1 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Entra el segon número");
+                        int num2 = Convert.ToInt32(Console.ReadLine());
+                        int maxim = MaximCalcul(num1, num2);
+                        Console.Clear();
+                        Console.WriteLine($"El numero més gran entre {num1} i {num2} és {maxim}");
+                        Return();
+                        break;
+                    case '2':
+                        Console.Clear();
+                        Console.WriteLine("Entra el primer número");
+                        num1 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Entra el segon número");
+                        num2 = Convert.ToInt32(Console.ReadLine());
+                        int resultat = MCDCalcul(num1, num2);
+                        Console.Clear();
+                        Console.WriteLine($"El MCD de  {num1} i {num2} és {resultat}");
+                        Return();
+                        break;
+                    case '3':
+                        Console.Clear();
+                        Console.WriteLine("Entra el primer número");
+                        num1 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Entra el segon número");
+                        num2 = Convert.ToInt32(Console.ReadLine());
+                        resultat = MCMCalcul(num1, num2);
+                        Console.Clear();
+                        Console.WriteLine($"El MCM de  {num1} i {num2} és {resultat}");
+                        Return();
+                        break;
+                    case '4':
+                        Console.Clear();
+                        Console.WriteLine("Entra un número");
+                        num1 = Convert.ToInt32(Console.ReadLine());
+                        resultat = FactorialCalcul(num1);
+                        Console.Clear();
+                        Console.WriteLine($"El factorial de {num1} és {resultat}");
+                        Return();
+                        break;
+                    case '5':
+                        Console.Clear();
+                        Console.WriteLine("Escriu dos numeros dels que es faran una operacio combinatoria");
+                        num1 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Escriu el segon numero");
+                        num2 = Convert.ToInt32(Console.ReadLine());
+                        resultat = Combinatori(num1, num2);
+                        Console.Clear();
+                        Console.WriteLine($"El combinatori de  {num1} i {num2} és {resultat}");
+                        Return();
 
-                    break;
-                case '6':
-                    Console.Clear();
-                    Console.WriteLine("Escriu un numero del que es buscara el seu divisor major");
-                    num1 = Convert.ToInt32(Console.ReadLine());
-                    resultat = DivisorMajor(num1);
-                    Console.Clear();
-                    Console.WriteLine($"El divisor major de {num1} és {resultat}");
-                    Return();
-                    break;
-                case '7':
-                    Console.Clear();
-                    Console.WriteLine("Escriu un numero del que es veura si es primer");
-                    num1 = Convert.ToInt32(Console.ReadLine());
-                    Console.Clear();
-                    string esprimer = Primer(num1);
-                    Console.WriteLine($"El numero {num1} {esprimer} és primer");
-                    Return();
-                    break;
-                case '8':
-                    Console.Clear();
-                    Console.WriteLine("Escriu el nombre de primers numeros primers que vols veure");
-                    num1 = Convert.ToInt32(Console.ReadLine());
-                    string primers = Nprimers(num1);
-                    Console.Clear();
-                    Console.WriteLine($"Els {num1} numeros primers són {primers}");
-                    Return();
-                    break;
-                case 'q':
-                    Console.WriteLine("Adéu");
-                    break;
-                default:
-                    Console.WriteLine("Introdueix un valor correcte");
-                    break;
+                        break;
+                    case '6':
+                        Console.Clear();
+                        Console.WriteLine("Escriu un numero del que es buscara el seu divisor major");
+                        num1 = Convert.ToInt32(Console.ReadLine());
+                        resultat = DivisorMajor(num1);
+                        Console.Clear();
+                        Console.WriteLine($"El divisor major de {num1} és {resultat}");
+                        Return();
+                        break;
+                    case '7':
+                        Console.Clear();
+                        Console.WriteLine("Escriu un numero del que es veura si es primer");
+                        num1 = Convert.ToInt32(Console.ReadLine());
+                        Console.Clear();
+                        string esprimer = Primer(num1);
+                        Console.WriteLine($"El numero {num1} {esprimer} és primer");
+                        Return();
+                        break;
+                    case '8':
+                        Console.Clear();
+                        Console.WriteLine("Escriu el nombre de primers numeros primers que vols veure");
+                        num1 = Convert.ToInt32(Console.ReadLine());
+                        string primers = Nprimers(num1);
+                        Console.Clear();
+                        Console.WriteLine($"Els {num1} numeros primers són {primers}");
+                        Return();
+                        break;
+                    case 'q':
+                        Console.WriteLine("Adéu");
+                        break;
+                    default:
+                        Console.WriteLine("Introdueix un valor correcte");
+                        Return();
+                        Console.Clear();
+                        break;
+                }
             }
         }
 
@@ -126,8 +133,9 @@ namespace Menu_Matemàtic
                 Console.Write("\r");
                 Console.Write($"Tornant al menu : {i}s");
                 Thread.Sleep(1000);
-                i--;
+                i--;  
             }
+            Console.Clear();
         }
         static int MaximCalcul(int num1, int num2)
         {
