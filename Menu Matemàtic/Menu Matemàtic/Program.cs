bdbbcd5ac2a86f0272eb1e890df050050146eb3d
michelbernabeu <mibela2005@gmail.com>
2024-01-18 11:39:22 +0100

    Version arrgelada

--- Menu Matemàtic/Menu Matemàtic/Program.cs
+++ Menu Matemàtic/Menu Matemàtic/Program.cs
@@ -15,7 +15,16 @@ namespace Menu_Matemàtic
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("Benvingut al menu matemàtic!");
             Console.ResetColor();
-            
+            Console.WriteLine("1. Màxim");
+            Console.WriteLine("2. MCD");
+            Console.WriteLine("3. MCM");
+            Console.WriteLine("4. Factorial");
+            Console.WriteLine("5. Combinatori");
+            Console.WriteLine("6. MostrarDivisorMajor");
+            Console.WriteLine("7. EsPrimer");
+            Console.WriteLine("8. NPrimersPrimers");
+            Console.WriteLine("q. Sortir");
+            Console.Write("Entra un valor: ");
             opcio = Console.ReadKey().KeyChar;
             Console.WriteLine();
             switch (opcio)
@@ -62,42 +71,43 @@ namespace Menu_Matemàtic
                     Console.WriteLine($"El factorial de {num1} és {resultat}");
                     break;
                 case '5':
+                    Console.Clear();
                     Console.WriteLine("Escriu dos numeros dels que es faran una operacio combinatoria");
                     num1 = Convert.ToInt32(Console.ReadLine());
                     Console.WriteLine("Escriu el segon numero");
                     num2 = Convert.ToInt32(Console.ReadLine());
-
-                    Combinatori(num1, num2);
-
-                    Console.WriteLine(Combinatori(num1, num2));
+                    resultat = Combinatori(num1, num2);
+                    Console.Clear();
+                    Console.WriteLine($"El combinatori de  {num1} i {num2} és {resultat}");
+                    Return();
 
                     break;
                 case '6':
+                    Console.Clear();
                     Console.WriteLine("Escriu un numero del que es buscara el seu divisor major");
                     num1 = Convert.ToInt32(Console.ReadLine());
-
-                    DivisorMajor(num1);
-
-                    Console.WriteLine(DivisorMajor(num1));
-
+                    resultat = DivisorMajor(num1);
+                    Console.Clear();
+                    Console.WriteLine($"El divisor major de {num1} és {resultat}");
+                    Return();
                     break;
                 case '7':
+                    Console.Clear();
                     Console.WriteLine("Escriu un numero del que es veura si es primer");
                     num1 = Convert.ToInt32(Console.ReadLine());
-
-                    Primer(num1);
-
-                    Console.WriteLine(Primer(num1));
-
+                    Console.Clear();
+                    string esprimer = Primer(num1);
+                    Console.WriteLine($"El numero {num1} {esprimer} és primer");
+                    Return();
                     break;
                 case '8':
+                    Console.Clear();
                     Console.WriteLine("Escriu el nombre de primers numeros primers que vols veure");
                     num1 = Convert.ToInt32(Console.ReadLine());
-
-                    Nprimers(num1);
-
-                    Console.WriteLine(Nprimers(num1));
-
+                    string primers = Nprimers(num1);
+                    Console.Clear();
+                    Console.WriteLine($"Els {num1} numeros primers són {primers}");
+                    Return();
                     break;
                 case 'q':
                     Console.WriteLine("Adéu");
@@ -178,9 +188,10 @@ namespace Menu_Matemàtic
             }
             return divisormajor;
         }
-        static bool Primer(int num1)
+        static string Primer(int num1)
         {
             bool primer = true;
+            string resultat = "";
             for (int i = 2; i != num1; i++)
             {
                 if (num1 % i == 0)
@@ -188,7 +199,15 @@ namespace Menu_Matemàtic
                     primer = false;
                 }
             }
-            return primer;
+            if (primer == false)
+            {
+                resultat = "NO";
+            }
+            else if (primer == true)
+            {
+                resultat = "SI";
+            }
+            return resultat;
         }
 
         static string Nprimers(int num1)
